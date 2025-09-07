5fcf18665cc3273495504267c8e4f553d5042f2a
sajid ansari <luckyansari1234@gmail.com>
2025-09-08 02:44:53 +0530

    unfiltered

--- backend/src/Services/DSAGrind.Auth.API/obj/Debug/net8.0/DSAGrind.Auth.API.AssemblyInfo.cs
+++ backend/src/Services/DSAGrind.Auth.API/obj/Debug/net8.0/DSAGrind.Auth.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSAGrind.Auth.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+755a143bdc1d90379bf1d3329d6a17183a8e1f24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fdf4b1d456595c8dba859424f72797b95c72a49")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSAGrind.Auth.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSAGrind.Auth.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
